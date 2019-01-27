ca8df32bcc3535a664c726da8df35dc6ebbba613
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2019-01-27 15:47:38 +0100

    Update twitter-demo.js

--- twitter-demo.js
+++ twitter-demo.js
@@ -4,44 +4,51 @@ let twitter_helpers = require('./config/TwitterAuth');
 let mongoDB_helpers = require('./lib/mongo_helpers');
 
 let TwitterStreaming = twitter_helpers.twitterAuth.stream('statuses/filter', {
-    track: "big data,bigdata,apple,google,twitter"
+	language: "en",
+	track: "big data,bigdata,apple,google,twitter"
 });
+
 TwitterStreaming.on('tweet', function (tweet) {
-    if(tweet.id){
-        // console.log(tweet.text);
-        mongoDB_helpers.IndexTweetsIntoMongoDB("tweets", tweet);
+	if (tweet.id) {
+		// console.log(tweet.text);
+		mongoDB_helpers.IndexTweetsIntoMongoDB("tweets", tweet);
+	}
+});
+TwitterStreaming.on('error', function (error, code) {
+	console.log("TwitterStreaming error: " + error + ": " + code);
+});
 
-    }
-});
-TwitterStreaming.on('error', function(error, code) {
-    console.log("TwitterStreaming error: " + error + ": " + code);
-});
 TwitterStreaming.on('disconnect', function (response) {
-    // Handle a disconnection
-    today = new Date();
-    console.log("TwitterStreaming disconnect: ", today, response);
-    // process.exit();
+	// Handle a disconnection
+	today = new Date();
+	console.log("TwitterStreaming disconnect: ", today, response);
+	// process.exit();
 });
-TwitterStreaming.on('warning', function (response) {
-    today = new Date();
-    console.log("TwitterStreaming warning: ", today, response);
-});
-TwitterStreaming.on('connect', function (response) {
-    today = new Date();
-    console.log("TwitterStreaming connect: ", today);
-});
-TwitterStreaming.on('reconnect', function (request, response, connectInterval) {
-    today = new Date();
-    console.log("TwitterStreaming reconnect: ", today);
 
-    console.log("response", response.statusCode);
-    console.log("connectInterval", connectInterval);
+TwitterStreaming.on('warning', function (response) {
+	today = new Date();
+	console.log("TwitterStreaming warning: ", today, response);
 });
+
+TwitterStreaming.on('connect', function (response) {
+	today = new Date();
+	console.log("TwitterStreaming connect: ", today);
+});
+
+TwitterStreaming.on('reconnect', function (request, response, connectInterval) {
+	today = new Date();
+	console.log("TwitterStreaming reconnect: ", today);
+
+	console.log("response", response.statusCode);
+	console.log("connectInterval", connectInterval);
+});
+
 TwitterStreaming.on('user_event', function (eventMsg) {
-    today = new Date();
-    console.log("TwitterStreaming user_event: ", today);
+	today = new Date();
+	console.log("TwitterStreaming user_event: ", today);
 });
+
 TwitterStreaming.on('end', function (response) {
-    today = new Date();
-    console.log("TwitterStreaming end: ", today, response);
+	today = new Date();
+	console.log("TwitterStreaming end: ", today, response);
 });
